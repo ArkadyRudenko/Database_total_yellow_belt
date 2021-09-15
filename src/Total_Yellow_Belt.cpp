2edde646d43230d9f10b343ab5c9983df668b505
ArkadyRudenko <rudenko.arkashaa@gmail.com>
2021-09-15 11:25:49 +0600

    Add_CMakeLists.txt

---
+++ src/Total_Yellow_Belt.cpp
@@ -0,0 +1,34 @@
+﻿#include "date.h"
+#include "condition_parser.h"
+#include "node.h"
+#include "test_runner.h"
+#include "command.h"
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include "database.h"
+
+#include "unit_tests.h"
+
+using namespace std;
+
+int main() {
+    TestAll();
+
+    Database db;
+
+    for (string line; getline(cin, line); ) {
+        istringstream is(line);
+
+        string command;
+        is >> command;
+        InputHandler ih;
+        if (!ih.handleInput(command, is, db, cout)) {
+            continue;
+        }      
+    }
+
+    return 0;
+}
+
---
+++ src/node.cpp
@@ -0,0 +1,59 @@
+#include "node.h"
+
+bool EmptyNode::Evaluate(const Date& date, const string& event) const  {
+	return true;
+}
+
+DateComparisonNode::DateComparisonNode(Comparison cmp, const Date& d) 
+    : date_(d), cmp_(cmp){};
+
+bool DateComparisonNode::Evaluate(const Date& date, const string& event)  const {
+    return СomparisonStringOrEvent(cmp_, date_, date);
+}
+
+EventComparisonNode::EventComparisonNode(Comparison cmp, const string& event)
+    : event_(event), cmp_(cmp) {}
+
+bool EventComparisonNode::Evaluate(const Date& data, const string& event) const {
+    return СomparisonStringOrEvent(cmp_, event_, event);
+}
+
+LogicalOperationNode::LogicalOperationNode(
+	LogicalOperation lo, shared_ptr<Node> left, shared_ptr<Node> right) 
+    : lo_(lo), left_(left), right_(right) {}
+
+bool LogicalOperationNode::Evaluate(const Date& date, const string& event) const{
+	if (lo_ == LogicalOperation::And) {
+	    return left_->Evaluate(date, event) && right_->Evaluate(date, event);
+    } else {
+		return left_->Evaluate(date, event) || right_->Evaluate(date, event);
+    }
+}
+
+template<typename T>
+bool СomparisonStringOrEvent(Comparison cmp_,const T& lhs,const T& rhs) {
+        if (cmp_ == Comparison::Equal) {
+		    return lhs == rhs;
+	    }
+        else if(cmp_ == Comparison::NotEqual){
+		    return lhs != rhs;
+	    }
+        else if(cmp_ == Comparison::NotEqual){
+		    return lhs != rhs;
+	    }
+        else if(cmp_ == Comparison::LessOrEqual){
+		    return lhs >= rhs;
+	    }
+        else if(cmp_ == Comparison::Less){
+		    return lhs > rhs;
+	    }
+        else if(cmp_ == Comparison::GreaterOrEqual){
+		    return lhs <= rhs;
+	    }
+        else if(cmp_ == Comparison::Greater){
+		    return lhs < rhs;
+	    }
+        else {
+            throw logic_error("Unknown comparison token");
+        }
+}
